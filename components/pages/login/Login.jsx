d2b292679d1afe3f5ecc7201f03b83d87029c125
rizalyoga <ach.rizal.yogaswara@gmail.com>
2022-05-05 22:18:47 +0700

    add register page

--- components/pages/login/Login.jsx
+++ components/pages/login/Login.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import styles from "@/styles/form.module.css";
 import Layout from "@/layout/Layout";
+import { useRouter } from "next/router";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
+  const router = useRouter();
+
+  const handleLogin = (e) => {
     e.preventDefault();
     console.log(username);
     console.log(password);
+    router.push("/home");
   };
 
   return (
@@ -17,7 +21,7 @@ const Login = () => {
       <div className="container">
         <div className={styles["form-container"]}>
           <h1>Login Form</h1>
-          <form className={styles.glass} onSubmit={handleSubmit}>
+          <form className={styles.glass} onSubmit={handleLogin}>
             <div className={styles.username}>
               <label htmlFor="username">Username</label>
               <input type="text" placeholder="username" onChange={(e) => setUsername(e.target.value)} required />
@@ -27,6 +31,9 @@ const Login = () => {
               <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} required />
             </div>
             <div className={styles.submit}>
+              <p>
+                Don't have an account yet ?<a href="/register"> Register</a>
+              </p>
               <input type="submit" />
             </div>
           </form>
--- components/pages/register/Register.jsx
+++ components/pages/register/Register.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import Layout from "@/layout/Layout";
 import styles from "@/styles/form.module.css";
+import { useRouter } from "next/router";
 
 const Register = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
+  const router = useRouter();
+
+  const handleRegister = (e) => {
     e.preventDefault();
     console.log(username);
     console.log(email);
     console.log(password);
+    router.push("/login");
   };
 
   return (
@@ -19,7 +23,7 @@ const Register = () => {
       <div className="container">
         <div className={styles["form-container"]}>
           <h1>Register Form</h1>
-          <form className={styles.glass} onSubmit={handleSubmit}>
+          <form className={styles.glass} onSubmit={handleRegister}>
             <div className={styles.username}>
               <label htmlFor="username">Username</label>
               <input type="text" placeholder="username" onChange={(e) => setUsername(e.target.value)} required />
@@ -33,6 +37,9 @@ const Register = () => {
               <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} required />
             </div>
             <div className={styles.submit}>
+              <p>
+                Alredy have an account ?<a href="/login"> Login</a>
+              </p>
               <input type="submit" />
             </div>
           </form>
