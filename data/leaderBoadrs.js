eafcf926b76a012a734f3a9b6d54687831d33124
rizalyoga <ach.rizal.yogaswara@gmail.com>
2022-05-11 10:53:29 +0700

    add create leaderboard

--- data/leaderBoadrs.js
+++ data/leaderBoadrs.js
@@ -1,3 +1,5 @@
+import getToken from "../utils/getCookies";
+
 export const getDataByHero = async (nameCharacter) => {
   const response = await fetch(`https://thrive-project-be.herokuapp.com/leaderboards?hero=${nameCharacter}`);
   const data = await response.json();
@@ -11,5 +13,26 @@ export const getDataByVillain = async (nameCharacter) => {
   const data = await response.json();
 
   //   console.log(data);
+  return data;
+};
+
+export const createNewLeaderboard = async (heroName, villainName) => {
+  const token = getToken();
+  const score = JSON.parse(localStorage.getItem(`${heroName}VS${villainName}`));
+  const payload = {
+    hero: heroName,
+    villain: villainName,
+    score: score.heroHP,
+  };
+
+  const response = await fetch(`https://thrive-project-be.herokuapp.com/leaderboards`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` },
+    body: JSON.stringify(payload),
+  });
+
+  const data = await response.json();
+
+  console.log(data);
   return data;
 };
--- pages/share/[idLeaderboard].jsx
+++ pages/share/[idLeaderboard].jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import SharePage from "@/components/pages/sharePage/SharePage";
 
-function SharesPage() {
+const SharesPage = () => {
   return (
     <>
       <SharePage />
     </>
   );
-}
+};
 
 export default SharesPage;
