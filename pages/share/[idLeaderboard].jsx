f9036ae75412e63e8ff678fb311bbbec1a8e13c2
rizalyoga <ach.rizal.yogaswara@gmail.com>
2022-05-12 11:21:42 +0700

    check ISR

--- pages/share/[idLeaderboard].jsx
+++ pages/share/[idLeaderboard].jsx
@@ -18,7 +18,7 @@ export async function getStaticPaths() {
 
   return {
     paths: allPathId,
-    fallback: false, // true or 'blocking'
+    fallback: "blocking", // true, false or 'blocking'
   };
 }
 
@@ -35,7 +35,7 @@ export async function getStaticProps(context) {
     // Next.js will attempt to re-generate the page:
     // - When a request comes in
     // - At most once every 10 seconds
-    revalidate: 5, // In seconds
+    revalidate: 1, // In seconds
   };
 }
 
