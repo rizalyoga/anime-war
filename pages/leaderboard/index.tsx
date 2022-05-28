9dca95f2c7498b71e7e5c4385da0239d46e85d89
rizalyoga <ach.rizal.yogaswara@gmail.com>
2022-05-28 22:28:33 +0700

    migrate leaderPage on main pages

--- pages/leaderboard/index.tsx
+++ pages/leaderboard/index.tsx
@@ -1,22 +1,29 @@
 import React from "react";
 import LeaderBoards from "@/components/pages/leaderBoards/LeaderBoards";
+import {DataLeaderboards} from "./interfacesLeaderboard"
 
-const Leaderboard = ({ data }) => {
+interface PropsLeaderboard {
+  propsLeaderboard: DataLeaderboards[];
+}
+
+const Leaderboard = ({ propsLeaderboard }:PropsLeaderboard) => {
+  
   return (
     <>
-      <LeaderBoards data={data} />
+      <LeaderBoards data={propsLeaderboard} />
     </>
   );
 };
-export async function getServerSideProps(context) {
+
+export async function getServerSideProps (context:any) {
   const query = context.query;
   // const token = req.cookies.userAuth;
 
   if (!query.filter) {
     const response = await fetch(`https://thrive-project-be.herokuapp.com/gametags`);
 
-    const data = await response.json();
-    return { props: { data } };
+    const propsLeaderboard = await response.json();
+    return { props: { propsLeaderboard } };
   }
 
   // Pass data to the page via props
